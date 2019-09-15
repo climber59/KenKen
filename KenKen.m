97014581e0c9ca8be6b1eda6d5d714e7e2d6fa05
climber59 <42081329+climber59@users.noreply.github.com>
2019-09-15 17:26:21 -0400

    Update KenKen.m
    attempt to fix whatever I messed up from not understanding github

--- KenKen.m
+++ KenKen.m
@@ -134,7 +134,7 @@ function [] = KenKen()
 	
 	% Checks if the puzzle has been completed
 	function [won] = winCheck()
-		won = nnz(userGrid)==n^2; %eveything filled in
+		won = sum(sum(~isnan(userGrid)))==n^2; %everything filled in
 		if ~won
 			return
 		end
@@ -236,7 +236,7 @@ function [] = KenKen()
 		
 		% Entering/removing notes
 		if noteMode
-			userGrid(r,c) = 0; % remove any "big" numbers
+			userGrid(r,c) = nan; % remove any "big" numbers
 			textGrid(r,c).String = '';
 			if ~isnan(num)% change a specific number
 				if notesGrid(r,c).String{cellRow}(strInds(1)) == ' '
@@ -298,7 +298,7 @@ function [] = KenKen()
 		blob = blobs(ind);
 		count = 0;
 		for i = 1:blob.UserData.size % count how many boxes in the blob are filled
-			count = count + (0~=userGrid(blob.UserData.rc(i,1),blob.UserData.rc(i,2)));
+			count = count + ~isnan(userGrid(blob.UserData.rc(i,1),blob.UserData.rc(i,2)));
 		end
 		
 		if count~=blob.UserData.size % only check the math when the blob is finished
@@ -348,7 +348,7 @@ function [] = KenKen()
 	
 	% Restarts the puzzle. Clears all notes and 'big' numbers
 	function [] = restart(~,~)
-		userGrid = zeros(n);
+		userGrid = nan(n);
 		for r = 1:n
 			for c = 1:n
 				textGrid(r,c).String = ' ';
@@ -375,7 +375,7 @@ function [] = KenKen()
 		noteMode = false;
 		
 		axis(1+[0 n 0 n])
-		userGrid = zeros(n);
+		userGrid = nan(n);
 		patchGrid(n);
 		numGrid = gridGen(n);
 		blobGen();
@@ -479,7 +479,7 @@ function [] = KenKen()
 	function [] = allNotesFcn(~,~)
 		for i = 1:n
 			for j = 1:n
-				if userGrid(i,j) == 0 && isempty(regexp([notesGrid(i,j).String{:}],'\d','once')) %~any(arrayfun(fcn,notesGrid(i,j)))
+				if isnan(userGrid(i,j)) && isempty(regexp([notesGrid(i,j).String{:}],'\d','once')) %~any(arrayfun(fcn,notesGrid(i,j)))
 					notesGrid(i,j).String = notesGrid(1,1).UserData.all;
 					updateNotes(0,0,i,j);
 				end
@@ -549,7 +549,7 @@ function [] = KenKen()
 	function [] = numSelection(src, ~, ind)
 		arrayOptions.SelectedObject = arrayCustom;
 		num = round(str2num(src.String));
-		if isempty(num) || isnan(num) || isinf(num) || num <=0
+		if isempty(num) || isnan(num) || isinf(num) %|| num <=0
 			num = nextNewNum();
 		end
 		src.String = num2str(num);
@@ -708,7 +708,7 @@ function [] = KenKen()
 	function [] = showNums() %#ok<DEFNU>
 		for r = 1:n
 			for c = 1:n
-				text(c+0.5,r+0.5,num2str(numGrid(r,c)),'FontSize',10,'HorizontalAlignment','center');
+				text(c+0.5,r+0.5,num2str(theNums(numGrid(r,c))),'FontSize',10,'HorizontalAlignment','center');
 			end
 		end
 	end
@@ -748,7 +748,7 @@ function [] = KenKen()
 				fs = fs * 0.9;
 			end
 		end
-		none = regexprep(noteString,'\d',' '); % get a blank copy of the string with only spaces
+		none = regexprep(noteString,'-|\d',' '); % get a blank copy of the string with only spaces
 		
 		for r = 1:n
 			for c = 1:n
@@ -788,7 +788,7 @@ function [] = KenKen()
 		for i = 1:length(blobs)
 			% 5 bools, [n + - * /] (n refers to a single square)
 			s = blobs(i).UserData.size;
-			num = numGrid(sub2ind([n n], blobs(i).UserData.rc(:,1), blobs(i).UserData.rc(:,2)));
+			num = theNums(numGrid(sub2ind([n n], blobs(i).UserData.rc(:,1), blobs(i).UserData.rc(:,2))));
 			if s==1
 				blobs(i).UserData.op = 1; % single num
 			elseif s==2
@@ -890,7 +890,7 @@ function [] = KenKen()
 	% not needed
 	function [g] = gridGen(n)
 		g = zeros(n);
-		g(:,1) = theNums(randperm(n));
+		g(:,1) = randperm(n);
 		g = recGen(g,1,2);
 	end
 	
@@ -908,7 +908,7 @@ function [] = KenKen()
 		end
 		good = false;
 		w = nonzeros(unique([grid(:,c),grid(r,:)'])); %all blocked options
-		e = theNums;
+		e = 1:n;
 		for i = 1:length(w)
 			e(e == w(i)) = []; % remove blocked options
 		end
